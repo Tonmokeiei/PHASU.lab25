0471f432638308164b2bd1688ce81215bcfee946
Tonmokeiei <tonmoke2546@gmail.com>
2023-03-14 01:41:50 +0700

    25.2completed

--- lab25_2.cpp
+++ lab25_2.cpp
@@ -1,31 +1,111 @@
 #include<iostream>
-#include<list>
-#include<algorithm>
-
 using namespace std;
 
-void printList(list<string> s){
-	list<string>::iterator i;
-	for( i = s.begin(); i != s.end(); i++){
-		cout << *i << " ";
-	}		
-	cout << "\n";	
+
+class Node{
+    public:
+    	int data;
+    	Node *next;
+    	~Node();
+};
+
+class List{
+	public:
+		Node *root;
+		int size;
+		void show();
+		void append(int);
+		void insert(int,int);
+		void remove(int);
+};
+
+Node::~Node(){
+    cout << data << " was deleted.\n";
 }
 
-int main(){
+void List::insert(int d,int idx){	
+	Node *n = new Node;
+	n->data = d;
 	
-	list<string> line_up;
-	list<string>::iterator loc;
-	
-	line_up.push_back("Alice");
-	line_up.push_back("Bob");
-	
-	loc = find(line_up.begin(),line_up.end(),"Bob");
-	line_up.insert(loc,"Oscar");
-	
-	//Write your code here
-	
-	printList(line_up);
-		
-	return 0;
+	if(idx == 0){
+		n->next = root;
+		root = n;
+		return;
+	}
+	Node *current = root;
+	for(int i = 0; i < idx-1;i++){
+		current = current->next;
+	}
+	n->next = current->next;
+	current->next = n;		
+}
+
+void List::show(){
+	Node *current = root;
+	cout << current->data << " ";	
+	while(current->next){
+		current = current->next;
+		cout << current->data << " ";
+	}	
+}
+
+void List::append(int d){	
+	Node *n = new Node;
+	n->data = d; n->next = NULL;
+	if(root == NULL) root = n;
+	else{
+		Node *current = root;
+		while(current->next){
+			current = current->next;
+		}
+		current->next = n;
+	}
+	size++;
+}
+
+//Write List::remove() here
+void List::remove(int index)
+{	
+	size--;
+	Node *current = root;
+	if (index == 0)
+	{
+		root = root->next;
+		delete current;
+	}
+	else
+	{
+		for (int i = 0; i < index - 1; i++) current = current->next;
+		Node *hereN = current->next;
+		current->next = hereN->next;
+		delete hereN;
+	}
+}
+
+int main()
+{
+	List myList = {0,0};
+	myList.append(5);
+	myList.append(7);
+	myList.append(11);
+	myList.append(4);
+	myList.append(12);
+	myList.append(45);
+
+	myList.insert(29,2);
+	myList.insert(33,3);
+	myList.insert(77,5);
+	myList.insert(69,0);
+	myList.show();
+	cout << "\n"; 
+
+	myList.remove(2);
+	myList.show();
+	cout << "\n"; 
+	myList.remove(4);
+	myList.show();
+	cout << "\n";
+	myList.remove(0);
+	myList.show();
+	cout << "\n";
 }
